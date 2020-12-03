b5f5950a73c5fabbf06148840d2e72fca4bf15db
eloi <eloi@bahuet.com>
2020-12-03 18:20:26 +0100

    part2

--- day3.py
+++ day3.py
@@ -1,5 +1,6 @@
 
 import os.path
+from functools import reduce
 
 
 def get_input():
@@ -10,8 +11,7 @@ def get_input():
     return data
 
 
-def part1():
-    data = get_input()
+def part1(data):
     tree_count = 0
     x = 0
     for line in data:
@@ -20,10 +20,24 @@ def part1():
         x = (x + 3) % len(line)
     return tree_count
 
-def part2():
-    pass
 
+def part2(data):
+    # lets do it in one scan for fun
+    slopes_data = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+
+    slopes = [{"right": s[0], "down": s[1], 'x': 0, 'tree_count': 0} for s in slopes_data]
+    y = 0
+
+    for line in data:
+        for slope in slopes:
+            if y % slope["down"] == 0:
+                if line[slope['x']] == '#':
+                    slope['tree_count'] += 1
+                slope['x'] = (slope['x'] + slope['right']) % len(line)
+        y += 1
+    return reduce((lambda x, y: x * y), [slope['tree_count'] for slope in slopes])
 
 if __name__ == '__main__':
-    print(part1())
-    print(part2())
+    input_data = get_input()
+    print(part1(input_data))
+    print(part2(input_data))
