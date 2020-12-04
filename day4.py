c3e3373d2300738cc7c1593c57b240fcf75dd138
eloi <eloi@bahuet.com>
2020-12-04 20:34:27 +0100

    part 2

--- day4.py
+++ day4.py
@@ -2,6 +2,15 @@ import re
 import os.path
 #REQ_FIELDS = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid", "cid"]
 REQ_FIELDS = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
+FIELDS_REGEX_RULES = {
+    "byr": '19[2-9][0-9]|200[0-2]',
+    "iyr": r'201\d|2020',
+    "eyr": r'202\d|2030',
+    "hgt": '^(?:(?:1[5-8][0-9]|19[0-3])cm)$|^(?:(?:59|6[0-9]|7[0-6])in)$',
+    "hcl": '^#[a-f0-9]{6}$',
+    "ecl": 'amb|blu|brn|gry|grn|hzl|oth',
+    "pid": '^\d{9}$'
+}
 
 
 def get_input():
@@ -19,28 +28,40 @@ def extract_passport_values(string):
         try:
             fields_dict[field] = match.group(1)
         except (IndexError, AttributeError):
-            pass
+            fields_dict[field] = None
     return fields_dict
 
 
-def check_passport_validity(passport_values):
+def check_passport_info_complete(passport_values):
     for field in REQ_FIELDS:
         if passport_values[field] is None:
             return False
     return True
 
 
+def check_passport_info_valid(passport_values):
+    for field in REQ_FIELDS:
+        regex = FIELDS_REGEX_RULES[field]
+        if not re.search(regex, passport_values[field]):
+            return False
+    return True
+
+
 def part1(passports):
-    valid_passports_count = 0
+    complete_passport_count = 0
     for passport in passports:
         passport_values = extract_passport_values(passport)
-        if check_passport_validity(passport_values):
-            valid_passports_count += 1
-    return valid_passports_count
+        if check_passport_info_complete(passport_values):
+            complete_passport_count += 1
+    return complete_passport_count
 
-
-def part2(data):
-    pass
+def part2(passports):
+    valid_passport_count = 0
+    for passport in passports:
+        passport_values = extract_passport_values(passport)
+        if check_passport_info_complete(passport_values) and check_passport_info_valid(passport_values):
+            valid_passport_count += 1
+    return valid_passport_count
 
 
 if __name__ == '__main__':
