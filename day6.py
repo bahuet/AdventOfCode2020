c318cf28d8fa4c79d66181dd2174b5e627876ba3
eloi <eloi@bahuet.com>
2020-12-06 14:48:44 +0100

    make data input parsing more generic for p2

--- day6.py
+++ day6.py
@@ -1,19 +1,20 @@
 import os.path
-
+from functools import reduce
 
 def get_input(day):
-    input_path = os.path.join('inputs', str(day) +'.txt')
+    input_path = os.path.join('inputs', str(day) + '.txt')
     f = open(input_path, 'r')
-    data =[x.replace('\n', '') for x in f.read().split('\n\n')]
+    data = [x.split('\n') for x in f.read().split('\n\n')]
     return data
-
 
 
 def part1(data):
     sum = 0
     for d in data:
-        sum += len(set(d))
+        mergedlist = reduce(lambda x,y: x + y, d)
+        sum += len(set(mergedlist))
     return sum
+
 
 def part2(data):
     pass
