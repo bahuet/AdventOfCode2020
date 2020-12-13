cebd7685752da730a350d3c3b43a09954dbb33a6
eloi <eloi@bahuet.com>
2020-12-13 12:39:54 +0100

    dynamic programming

--- day10.py
+++ day10.py
@@ -8,11 +8,16 @@ def get_input(day):
         return data
 
 
-def count_differences(data):
+def get_sorted_adapters(data):
     adapters_sorted = sorted(data)
     # add outlet and adapter
     adapters_sorted.insert(0, 0)
     adapters_sorted.append(adapters_sorted[-1]+3)
+    return adapters_sorted
+
+
+def count_differences(data):
+    adapters_sorted = get_sorted_adapters(data)
     counts = {}
     for i in range(1, len(adapters_sorted)):
         diff = adapters_sorted[i] - adapters_sorted[i-1]
@@ -25,8 +30,23 @@ def part1(data):
     return(counts[1] * counts[3])
 
 
+def nb_of_paths(i, lst, hm):
+    if i == len(lst) - 1:
+        return 1
+    if i in hm:
+        return hm[i]
+    x = 0
+    for j in range(i+1, min(i+4, len(lst))):
+        if lst[j] - lst[i] <= 3:
+            x += nb_of_paths(j, lst, hm)
+    hm[i] = x
+    return x
+
 def part2(data):
-    pass
+    adapters = get_sorted_adapters(data)
+    hashmapu = {}
+    return nb_of_paths(0, adapters, hashmapu)
+
 
 
 if __name__ == '__main__':
