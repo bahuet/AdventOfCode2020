d5174422f17395724b9a15e23ba23922778d85ad
eloi <eloi@bahuet.com>
2020-12-23 19:48:06 +0100

    p1 complete

--- day17.py
+++ day17.py
@@ -11,38 +11,40 @@ def get_input(day, bTest=False):
 
 
 def init_next_cube(cube):
-    new_cube = {}
+    next_cube = {}
 
-    new_cube_min_z = min(cube.keys()) - 1
-    new_cube_max_z = max(cube.keys()) + 1
-    new_cube_h = len(cube[0]) + 2
-    new_cube_w = len(cube[0][0]) + 2
+    next_cube_min_z = min(cube.keys()) - 1
+    next_cube_max_z = max(cube.keys()) + 1
+    next_cube_h = len(cube[0]) + 2
+    next_cube_w = len(cube[0][0]) + 2
 
-    z_range = range(new_cube_min_z, new_cube_max_z + 1)
-    y_range = range(new_cube_h)
-    x_range = range(new_cube_w)
+    z_range = range(next_cube_min_z, next_cube_max_z + 1)
+    y_range = range(next_cube_h)
+    x_range = range(next_cube_w)
 
     for z in z_range:
-        new_cube[z] = []
+        next_cube[z] = []
         for _ in y_range:
-            new_cube[z].append([False] * new_cube_w)
-    return new_cube, z_range, y_range, x_range
+            next_cube[z].append([False] * next_cube_w)
+    return next_cube, z_range, y_range, x_range
 
 
-def get_points_info(x, y, z, cube):
+def get_points_info(z_next_cube, y_next_cube, x_next_cube, cube):
     active_neighbours_count = 0
     point_is_active = False
     probing_range = range(-1, 2)
-    probing_range2 = range(0, 3)
+
     for i in probing_range:
-        z_probe = z + i
-        for j in probing_range2:
-            y_probe = y + j
-            for k in probing_range2:
-                x_probe = x + k
+        z_probe = z_next_cube + i
+        for j in probing_range:
+            y_probe = y_next_cube + j
+            for k in probing_range:
+                x_probe = x_next_cube + k
                 try:
+                    if y_probe < 0 or x_probe < 0:
+                        raise IndexError
                     if cube[z_probe][y_probe][x_probe]:
-                        if z_probe == z and y_probe == y and x_probe == x:
+                        if z_probe == z_next_cube and y_probe == y_next_cube and x_probe == x_next_cube:
                             point_is_active = True
                         else:
                             active_neighbours_count += 1
@@ -51,8 +53,8 @@ def get_points_info(x, y, z, cube):
     return active_neighbours_count, point_is_active
 
 
-def next_state_is_active(x, y, z, cube):
-    active_neighbours_count, point_is_active = get_points_info(x, y, z, cube)
+def next_state_is_active(z_next_cube, y_next_cube, x_next_cube, cube):
+    active_neighbours_count, point_is_active = get_points_info(z_next_cube, y_next_cube, x_next_cube, cube)
 
     if active_neighbours_count == 3 or (active_neighbours_count == 2 and point_is_active):
         return True
@@ -64,21 +66,20 @@ def get_next_cube(cube):
     active_count = 0
     next_cube, z_range, y_range, x_range = init_next_cube(cube)
 
-    for z in z_range:
-        for y in y_range:
-            for x in x_range:
-                active_state = next_state_is_active(x, y, z, cube)
+    for z_next_cube in z_range:
+        for y_next_cube in y_range:
+            for x_next_cube in x_range:
+                active_state = next_state_is_active(z_next_cube, y_next_cube - 1, x_next_cube - 1, cube)
                 if active_state:
-                    next_cube[z][y][x] = active_state
+                    next_cube[z_next_cube][y_next_cube][x_next_cube] = active_state
                     active_count += 1
     return active_count, next_cube
 
 
 def part1(initial_slice):
     cube = {0: initial_slice}
-    for _ in range(1):
+    for _ in range(6):
         active_count, next_cube = get_next_cube(cube)
-        print(next_cube[0])
         cube = next_cube
     return active_count
 
@@ -89,6 +90,6 @@ def part2(initial_slice):
 
 if __name__ == '__main__':
     DAY = 17
-    input_data = get_input(DAY, True)
+    input_data = get_input(DAY, False)
     print(part1(input_data))
     print(part2(input_data))
