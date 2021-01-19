a5d7ece72f9ad23bbd8d0ba71fd389ac03077efa
eloi <eloi@bahuet.com>
2021-01-19 19:47:03 +0100

    unable

--- day24.py
+++ day24.py
@@ -74,25 +74,8 @@ class Floor:
     def get_black_count(self):
         return len([x for x in self.hm.values() if not x.white])
 
-    def get_scope(self):
-        x = [0, 0]
-        y = [0, 0]
-        z = [0, 0]
-        for tile in self.hm.values():
-            if tile.x < x[0]:
-                x[0] = tile.x
-            elif tile.x > x[1]:
-                x[1] = tile.x
-            if tile.y < y[0]:
-                y[0] = tile.y
-            elif tile.y > y[1]:
-                y[1] = tile.y
-            if tile.z < z[0]:
-                z[0] = tile.z
-            elif tile.z > z[1]:
-                z[1] = tile.z
-
-        return x, y, z
+    def get_max_radius(self):
+        return max([max(tile.x, tile.y, tile.z) for tile in self.hm.values()])
 
     def is_black(self, x, y, z):
         key = self.get_tile_key(x, y, z)
@@ -100,13 +83,18 @@ class Floor:
             return True
         return False
 
+    def get_tiles_ring(self, radius):
+        if radius == 0:
+            return [[0, 0, 0]]
+        ps = permutations(range(-radius, radius+1), 3)
+        return [[p[0], p[1], p[2]] for p in ps if sum(p) == 0 and radius == max(-1*min(p), max(p))]
+
     def get_adj_tiles_coords(self, x, y, z):
-        ps = permutations(range(-1, 2), 3)
-        return [[x + p[0], y + [1], z + p[2]] for p in ps]
+        return [[x + p[0], y + p[1], z + p[2]] for p in self.get_tiles_ring(1)]
 
     def get_adj_tiles_black_count(self, x, y, z):
         bcount = 0
-        for x_probe, y_probe, z_probe in self.get_adj_tiles_coords(x,y,z):
+        for x_probe, y_probe, z_probe in self.get_adj_tiles_coords(x, y, z):
             if self.is_black(x_probe, y_probe, z_probe):
                 bcount += 1
         return bcount
@@ -124,19 +112,24 @@ class Floor:
             if tile.will_flip:
                 tile.flip_color()
                 count += 1
+        # print(count)
 
     def do_art(self):
-        x_scope, y_scope, z_scope = self.get_scope()
-        for x in range(x_scope[0] - 1, x_scope[1] + 2):
-            for y in range(y_scope[0] - 1, y_scope[1] + 2):
-                for z in range(z_scope[0] - 1, z_scope[1] + 2):
-                    bcount = self.get_adj_tiles_black_count(x, y, z)
-                    is_white = not self.is_black(x, y, z)
-                    flip = self.should_flip(is_white, bcount)
-                    if flip:
-                        flip = self.get_tile_if_exists(Tile(x, y, z))
-                        flip.will_flip = True
-
+        max_r = self.get_max_radius()
+        print('max_r: ', max_r)
+        check_count = 0
+        for r in range(max_r + 2):
+            ring_tiles = self.get_tiles_ring(r)
+            print('len(ring_tiles): ', len(ring_tiles))
+            for x, y, z in ring_tiles:
+                check_count += 1
+                bcount = self.get_adj_tiles_black_count(x, y, z)
+                is_white = not self.is_black(x, y, z)
+                flip = self.should_flip(is_white, bcount)
+                if flip:
+                    flip = self.get_tile_if_exists(Tile(x, y, z))
+                    flip.will_flip = True
+        print('check_count:', check_count)
         self.apply_flipping()
 
 
@@ -152,16 +145,18 @@ def part1(data):
 
 
 def part2(floor):
-    for _ in range(10):
+    for i in range(100):
         floor.do_art()
+        print('Day ', i+1, ': ', floor.get_black_count())
     return floor.get_black_count()
 
+# 464 too low.
 # 12412 too high.
 
 
 if __name__ == '__main__':
     DAY = 24
-    input_data = get_input(DAY, False)
+    input_data = get_input(DAY, True)
     p1_floor = part1(input_data)
     print(p1_floor.get_black_count())
     print(part2(p1_floor))
