ef70cfa6d575c24ef3fbecfcf0ee0f4546384d54
eloi <eloi@bahuet.com>
2021-02-01 19:30:55 +0100

    vimp2

--- d1vim.py
+++ d1vim.py
@@ -3,9 +3,20 @@ with open('inputs/1.txt') as f:
 #p1
 comps = set()
 for x in d:
-    y = 2020 -x
+    y = 2020 - x
     if x in comps:
         print(x*y)
         break
     else:
         comps.add(y)
+#p2
+for xi, x in enumerate(d):
+    comps2 = set()
+    target = 2020 - x
+    for yi in range(xi+1, len(d)):
+        y = d[yi]
+        z = target - y
+        if z in comps2:
+            print(x * y * z)
+            break
+        comps2.add(y)
