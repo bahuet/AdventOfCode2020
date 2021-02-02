0a5c2a2dcd85e803be38b56cb107d5c0179829c4
eloi <eloi@bahuet.com>
2021-02-02 18:38:44 +0100

    d2p2vim

--- d2vim.py
+++ d2vim.py
@@ -2,8 +2,13 @@ import re
 with open('inputs/2.txt') as f:
     d = re.findall(r'(\d+)-(\d+)\s(\w):\s(\w+)',f.read())
 valid = 0
+valid2 = 0
 for cmin, cmax, L, pwd in d:
-    if int(cmin) <= pwd.count(L) <= int(cmax):
+    cmin = int(cmin)
+    cmax = int(cmax)
+    if cmin <= pwd.count(L) <= cmax:
         valid += 1
-print(valid)
+    if (pwd[cmin-1] == L) ^ (pwd[cmax-1] == L):
+        valid2 += 1
+print(valid, valid2)
 
