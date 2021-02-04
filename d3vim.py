5e87305dabbaeb4f40b9a2d250e37b920df415c7
eloi <eloi@bahuet.com>
2021-02-04 18:05:18 +0100

    d3vim

--- d3vim.py
+++ d3vim.py
@@ -8,3 +8,12 @@ for row, col in zip(range(height), count(0,3)):
     if d[row][col % width] == '#':
         trees += 1
 print(trees)
+slopes = ((1, 1), (1, 5), (1, 7), (2, 1))
+total = trees
+for down, right in slopes:
+    trees = 0
+    for row, col in zip(range(0, height, down), count(0,right)):
+        if d[row][col%width] == '#':
+            trees +=1
+    total *= trees
+print(total)
