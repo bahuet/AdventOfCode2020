c6cf99398e7537775d554027f7efb102300ca425
eloi <eloi@bahuet.com>
2021-02-19 18:51:11 +0100

    recursive DFS

--- d7vim.py
+++ d7vim.py
@@ -8,4 +8,9 @@ for line in d:
     inners = inner_exp.findall(inners)
     for _, inner in inners:
         contained_by[inner].append(outer)
-
+def count_can_contain(G, src, visited=set()):
+    for color in G[src]:
+        visited.add(color)
+        count_can_contain(G, color, visited)
+    return len(visited)
+print(count_can_contain(contained_by, 'shiny gold'))
