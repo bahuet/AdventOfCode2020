57331990c90b22a8e56f1b6eea97b1b4f2d93996
eloi <eloi@bahuet.com>
2021-03-23 21:06:26 +0100

    pretty hard

--- d13vim.py
+++ d13vim.py
@@ -5,6 +5,16 @@ best = float('inf')
 bestid = None
 for bid in buses:
     mod = bid - d[0]%bid
-    if  mod < best:
+    if mod < best:
         best, bestid = mod, bid
 print(best*bestid)
+from itertools import count
+from math import gcd
+buses2 = tuple((i,b) for (i,b) in enumerate(d[1]) if isinstance(b,int))
+t, step = buses2[0]
+for delta, period in buses2[1:]:
+    for t in count(t, step):
+        if (t+delta) % period == 0:
+            break
+    step = step * period // gcd(step, period)
+print(t)
