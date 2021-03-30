f7cb4b21843879edfdc882ba3cd083f29ed4817a
Eloi <eloi@bahuet.com>
2021-03-30 22:51:45 +0200

    nice

--- d14vim.py
+++ d14vim.py
@@ -4,8 +4,11 @@ import re
 rexp = re.compile('^mem\[(\d+)\] = (\d+)$')
 mem = {}
 for line in lines:
-    if line.starswith('mask'):
+    if line.startswith('mask'):
         mask = line[7:].rstrip()
+        and_mask = int(mask.replace('1','0').replace('X','1'),2)
+        or_mask = int(mask.replace('X','0'),2)
     else:
         addr, val = map(int, rexp.findall(line)[0])
-        
+        mem[addr] = val & and_mask | or_mask
+print(sum(mem.values()))
