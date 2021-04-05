48fe113f95ab411d3ca5e081ed38766ab8b22df2
Eloi <eloi@bahuet.com>
2021-04-05 18:08:44 +0200

    dict_comprehension

--- d15vim.py
+++ d15vim.py
@@ -1,18 +1,14 @@
 d = [6,13,1,15,2,0]
 def nth_num(n):
-    turn_rec = {}
-    turn = 1
-    while turn <= n :
-        try:
-            num = d[turn-1]
-        except IndexError:
-            if last_seen_turn is not None:
-                num = turn - last_seen_turn - 1 
-            else:
-                num = 0
-        last_seen_turn = turn_rec.get(num)
-        turn_rec[num] = turn
-        turn += 1
+    turn_rec = {num:turn for turn, num in enumerate(d[:-1], 1)}
+    prev = d[-1]
+    for turn in range(len(d)+1, n+1):
+        if prev in turn_rec:
+            num = turn - turn_rec[prev] - 1 
+        else:
+            num = 0
+        turn_rec[prev] = turn - 1 
+        prev = num
     return num
 print(nth_num(2020))
-print(nth_num(30000000))
+#print(nth_num(30000000))
